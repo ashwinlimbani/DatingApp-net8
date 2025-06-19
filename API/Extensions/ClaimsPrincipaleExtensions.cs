09983be352454b2f22c10054a8c397bfefba1116
ashwinlimbanee <ashwinlimbani23@gmail.com>
2025-06-19 15:43:45 +0530

    End of Section 13

--- API/Extensions/ClaimsPrincipaleExtensions.cs
+++ API/Extensions/ClaimsPrincipaleExtensions.cs
@@ -6,6 +6,11 @@ public static class ClaimsPrincipaleExtensions
 {
     public static string GetUsername(this ClaimsPrincipal user)
     {
-        return user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Cannot get username from token");
+        return user.FindFirstValue(ClaimTypes.Name) ?? throw new Exception("Cannot get username from token");
+    }
+
+    public static int GetUserId(this ClaimsPrincipal user)
+    {
+        return int.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier) ?? throw new Exception("Cannot get username from token"));
     }
 }
